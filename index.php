77591a034cd88a2d1c9dd57d4f3d81c578fec3d1
Gurami <guramxatiashvili@hotmail.com>
2020-02-22 12:49:09 +0100

    Update index.php

--- index.php
+++ index.php
@@ -1,2 +1,3 @@
 <?php include_once("index.html")?>
-#i used this one line on PHP code,because i could't deploy it on heroku without server side programming.Otherwise we don't need that at all.
+/* i used this one line on PHP code,because i could't deploy it on heroku without server side programming.
+Otherwise we don't need that at all. */
